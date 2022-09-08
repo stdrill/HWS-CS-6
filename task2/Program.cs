bbb8aa9781903712d07828d501a5106b3bbe5d17
stdrill <tanya23.96@mail.ru>
2022-09-08 21:28:21 +0300

    task2 added

---
+++ task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+void PrintArray (int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++){
+        for(int j = 0; j < matr.GetLength(1); j++){
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray (int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++){
+        for(int j = 0; j < matr.GetLength(1); j++){
+            matr[i, j] = i + j;
+        }
+    }   
+}
+
+Console.WriteLine("Enter M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
